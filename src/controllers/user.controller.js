e4f339689191d051dfcbd3e691418ac2746d3f17
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-03-05 13:44:03 -0300

    refactor: surround createUser method with try/catch block

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,20 +2,24 @@ const { userService } = require('../services');
 const { validateUserBody } = require('./utils/validateUserBody');
 
 const createUser = async (req, res) => {
-  const { error: validationError } = validateUserBody(req.body);
-  if (validationError) return res.status(400).json({ message: validationError.message });
-
-  const newUser = await userService.createUser(req.body);
-
-  if (newUser.error) {
-    const { status, message } = newUser.error;
-    return res.status(status).send({ message });
-  } 
-
-  const { email, password } = req.body;
-  const { token } = await userService.login(email, password);
-
-  return res.status(201).json({ token });
+  try {
+    const { error: validationError } = validateUserBody(req.body);
+    if (validationError) return res.status(400).json({ message: validationError.message });
+  
+    const newUser = await userService.createUser(req.body);
+  
+    if (newUser.error) {
+      const { status, message } = newUser.error;
+      return res.status(status).send({ message });
+    } 
+  
+    const { email, password } = req.body;
+    const { token } = await userService.login(email, password);
+  
+    return res.status(201).json({ token });
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
 };
 
 module.exports = {
